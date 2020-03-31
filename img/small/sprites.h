c78c5551d2ebe351ebcd5985b98c127bcd50ecc3
Veikko Soininen <veikko.soininen@gmail.com>
2020-03-31 19:55:05 +0300

    Update GBA eatables. Add second dynamic eatable

--- img/small/sprites.h
+++ img/small/sprites.h
@@ -3,12 +3,12 @@
 
 //======================================================================
 //
-//	sprites, 88x8@8, 
+//	sprites, 144x8@8, 
 //	+ palette 256 entries, not compressed
-//	+ 11 tiles not compressed
-//	Total size: 512 + 704 = 1216
+//	+ 18 tiles not compressed
+//	Total size: 512 + 1152 = 1664
 //
-//	Time-stamp: 2020-03-29, 17:20:54
+//	Time-stamp: 2020-03-31, 19:54:07
 //	Exported by Cearn's GBA Image Transmogrifier, v0.8.6
 //	( http://www.coranac.com/projects/#grit )
 //
@@ -17,8 +17,8 @@
 #ifndef GRIT_SPRITES_H
 #define GRIT_SPRITES_H
 
-#define spritesTilesLen 704
-extern const unsigned short spritesTiles[352];
+#define spritesTilesLen 1152
+extern const unsigned short spritesTiles[576];
 
 #define spritesPalLen 512
 extern const unsigned short spritesPal[256];
